b341e3559a80148b7f815e5566a42e1d1dd8dbd3
subodh <sktandon0121@gmail.com>
2018-12-24 01:27:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+
 
 class App extends Component {
+  
+  state = {
+    person : [
+      {name:  "Subodh",age:21},
+      {name:  "Manu",age:15},
+    ],
+    showPerson:false
+  }
+  
+  switchNameHandler = () => {
+    this.setState({person:[{name:  "Subodh Tandon",age:21},
+      {name:  "Manu Tandon",age:14},]})
+  }
+  nameChangeHandler = (event) => {
+    this.setState({
+      person:[
+        {name:"Harshu" , age:12},
+        {name:event.target.value,age:17}
+    ]
+    });
+  }
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({
+      showPerson:!doesShow
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>I am using React js </h1>
+        <p>this is working !!!</p>
+        <button onClick={this.togglePersonHandler}>Switch</button>
+        { 
+          this.state.showPerson === true ?
+          <div>
+            <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+            <Person name={this.state.person[1].name} age={this.state.person[1].age}
+              changed={this.nameChangeHandler}
+            >Hobbies: Bike Racing</Person>
+          </div>
+          : null
+        }
+        
       </div>
     );
   }
